8d08ce12f570d7715dd7bd2862fb8e48677a20e1
Andrii <sevenander@gmail.com>
2014-12-20 20:12:10 +0200

    color picker added

--- app/src/main/java/qsoft/hacktbilisi/app/actvities/EditLessonActivity.java
+++ app/src/main/java/qsoft/hacktbilisi/app/actvities/EditLessonActivity.java
@@ -2,11 +2,14 @@ package qsoft.hacktbilisi.app.actvities;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 import qsoft.hacktbilisi.app.R;
 import qsoft.hacktbilisi.app.utils.Utils;
+import qsoft.hacktbilisi.app.utils.it.gmariotti.android.example.colorpicker.calendarstock.ColorPickerDialog;
+import qsoft.hacktbilisi.app.utils.it.gmariotti.android.example.colorpicker.calendarstock.ColorPickerSwatch;
 
 /**
  * Created by andrii on 20.12.14.
@@ -50,22 +53,22 @@ public class EditLessonActivity extends Activity implements View.OnClickListener
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fl_new_lesson_color:
-//                ColorPickerDialog colorcalendar = ColorPickerDialog.newInstance(
-//                        R.string.dialog_color_picker_title,
-//                        Utils.colorChoice(this), selectedColor, 5,
-//                        ColorPickerDialog.SIZE_SMALL);
-//
-//                colorcalendar.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
-//                    @Override
-//                    public void onColorSelected(int color) {
-//                        selectedColor = color;
-//                        stringSelectedColor = Utils.colorToString(selectedColor);
-//                        flNewLessonColor.setBackgroundColor(Color.parseColor(stringSelectedColor));
-//                    }
-//
-//                });
-//
-//                colorcalendar.show(getFragmentManager(), "cal");
+                ColorPickerDialog colorcalendar = ColorPickerDialog.newInstance(
+                        R.string.dialog_color_picker_title,
+                        Utils.colorChoice(this), selectedColor, 5,
+                        ColorPickerDialog.SIZE_SMALL);
+
+                colorcalendar.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int color) {
+                        selectedColor = color;
+                        stringSelectedColor = Utils.colorToString(selectedColor);
+                        flNewLessonColor.setBackgroundColor(Color.parseColor(stringSelectedColor));
+                    }
+
+                });
+
+                colorcalendar.show(getFragmentManager(), "cal");
                 break;
         }
     }
