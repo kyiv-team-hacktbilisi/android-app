0c0dc38cf14fd90b26935868209eecf7082e960e
Andrii <sevenander@gmail.com>
2014-12-20 15:04:45 +0200

    lesson values set to view

--- app/src/main/java/qsoft/hacktbilisi/app/actvities/LessonPreviewActivity.java
+++ app/src/main/java/qsoft/hacktbilisi/app/actvities/LessonPreviewActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import qsoft.hacktbilisi.app.R;
+import qsoft.hacktbilisi.app.pojo.Lesson;
 
 /**
  * Created by andrii on 20.12.14.
@@ -15,7 +17,14 @@ public class LessonPreviewActivity extends Activity implements View.OnClickListe
 
     private Context context;
 
+    private TextView tvLessonName;
+    private TextView tvAudience;
+    private TextView tvTeacher;
+    private TextView tvType;
+    private TextView tvTime;
     private Button bShowComments;
+
+    private Lesson lesson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,11 @@ public class LessonPreviewActivity extends Activity implements View.OnClickListe
         setContentView(R.layout.activity_lesson_preview);
 
         context = this;
+        loadLesson();
+    }
+
+    private void loadLesson() {
+        // todo load lesson
     }
 
     @Override
@@ -33,10 +47,24 @@ public class LessonPreviewActivity extends Activity implements View.OnClickListe
     }
 
     private void initViews() {
+        tvLessonName = (TextView) findViewById(R.id.tv_lesson_preview_name);
+        tvTeacher = (TextView) findViewById(R.id.tv_teacher);
+        tvAudience = (TextView) findViewById(R.id.tv_audience);
+        tvType = (TextView) findViewById(R.id.tv_lesson_type);
+        tvTime = (TextView) findViewById(R.id.tv_time);
         bShowComments = (Button) findViewById(R.id.b_show_comments);
     }
 
     private void setupViews() {
+        if (lesson != null) {
+            tvLessonName.setText(lesson.getName());
+            tvTeacher.setText(lesson.getTeacher());
+            tvAudience.setText(lesson.getAudience());
+            tvType.setText(lesson.getType());
+            //todo format date
+//            tvTime.setText(lesson.getStartTime());
+        }
+
         bShowComments.setOnClickListener(this);
     }
 
