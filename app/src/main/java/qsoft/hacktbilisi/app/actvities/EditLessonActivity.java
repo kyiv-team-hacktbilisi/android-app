b848f1a21a9b7c4baaf6c0e246e390ae166a142e
Andrii <sevenander@gmail.com>
2014-12-20 20:19:11 +0200

    time picker added

--- app/src/main/java/qsoft/hacktbilisi/app/actvities/EditLessonActivity.java
+++ app/src/main/java/qsoft/hacktbilisi/app/actvities/EditLessonActivity.java
@@ -1,15 +1,23 @@
 package qsoft.hacktbilisi.app.actvities;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TimePicker;
 import qsoft.hacktbilisi.app.R;
 import qsoft.hacktbilisi.app.utils.Utils;
 import qsoft.hacktbilisi.app.utils.it.gmariotti.android.example.colorpicker.calendarstock.ColorPickerDialog;
 import qsoft.hacktbilisi.app.utils.it.gmariotti.android.example.colorpicker.calendarstock.ColorPickerSwatch;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by andrii on 20.12.14.
@@ -19,6 +27,8 @@ public class EditLessonActivity extends Activity implements View.OnClickListener
     private Context context;
 
     private FrameLayout flNewLessonColor;
+    private Button bStartTime;
+
     private int selectedColor;
     private String stringSelectedColor;
 
@@ -40,10 +50,12 @@ public class EditLessonActivity extends Activity implements View.OnClickListener
     }
 
     private void initViews() {
+        bStartTime = (Button) findViewById(R.id.b_start_time);
         flNewLessonColor = (FrameLayout) findViewById(R.id.fl_new_lesson_color);
     }
 
     private void setupViews() {
+        bStartTime.setOnClickListener(this);
         flNewLessonColor.setOnClickListener(this);
         selectedColor = Utils.colorChoice(this)[0];
         stringSelectedColor = Utils.colorToString(selectedColor);
@@ -70,6 +82,28 @@ public class EditLessonActivity extends Activity implements View.OnClickListener
 
                 colorcalendar.show(getFragmentManager(), "cal");
                 break;
+            case R.id.b_start_time:
+                timePickerDialog(bStartTime);
+                break;
         }
+    }
+
+    private void timePickerDialog(final Button button) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        TimePickerDialog tpd = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker view, int hour, int minute) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(Calendar.HOUR_OF_DAY, hour);
+                calendar.set(Calendar.MINUTE, minute);
+
+                DateFormat df = new SimpleDateFormat("HH:mm", Locale.getDefault());
+                String formattedDate = df.format(calendar.getTime());
+                button.setText(formattedDate);
+            }
+        }, hour, minute, true);
+        tpd.setTitle("Choose Time");
+        tpd.show();
     }
 }
